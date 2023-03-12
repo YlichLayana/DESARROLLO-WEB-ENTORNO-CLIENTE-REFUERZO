2e82f427ab1938f3f90a83ac31436bed0b8e027d
YlichLayana <ylichlayana@gmail.com>
2023-03-12 14:19:09 +0100

    he modificado y terminado el ejercicio 7

--- 4 EjercicioExtras/Retos/07BuscandoEnAlmacen.js
+++ 4 EjercicioExtras/Retos/07BuscandoEnAlmacen.js
@@ -5,19 +5,53 @@
 //Todo: La función debe devolver un booleano que indique si se encuentra el string como valor en algún nivel del objeto.
 
 const almacen = {
-    'estanteria1': {
-      'cajon1': {
-        'producto1': 'coca-cola',
-        'producto2': 'fanta',
-        'producto3': 'sprite'
-      }
+  estanteria1: {
+    cajon1: {
+      producto1: "coca-cola",
+      producto2: "fanta",
+      producto3: "sprite",
     },
-    'estanteria2': {
-      'cajon1': 'vacio',
-      'cajon2': {
-        'producto1': 'pantalones',
-        'producto2': 'camiseta' // <- ¡Está aquí!
-      }
+  },
+  estanteria2: {
+    cajon1: "vacio",
+    cajon2: {
+      producto1: "pantalones",
+      producto2: "camiseta", // <- ¡Está aquí!
+    },
+  },
+};
+
+const otroAlmacen = {
+  'baul': {
+    'fondo': {
+      'objeto': 'cd-rom',
+      'otro-objeto': 'disquette',
+      'otra-cosa': 'mando'
     }
+  }
 }
 
+let arrayOpcional = [];
+
+function iterarAlmacen(object) { 
+  for (const key in object) {
+    const element = object[key];
+    if (typeof element === "object") {
+      iterarAlmacen(element);
+    } else {
+      arrayOpcional.push(element);
+    }
+  }
+  return arrayOpcional;
+}
+
+function contains(store, product) {
+  if (iterarAlmacen(store).includes(product)) {
+    return true;
+  }
+
+  return false;
+}
+
+console.log(contains(almacen, "camiseta"));
+console.log(contains(otroAlmacen, 'gameboy'));
